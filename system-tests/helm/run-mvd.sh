d804dbfcaa0f5a58ebd7e99c1cd6e7cd990b7926
farhin23 <farhin.jahan@isst.fraunhofer.de>
2024-03-05 14:11:49 +0100

    Implement configuration for debugging purpose

--- system-tests/helm/run-mvd.sh
+++ system-tests/helm/run-mvd.sh
@@ -9,13 +9,13 @@ echo "installing registration-service..."
 helm install registration-service ./helm-charts/registration-service
 
 echo "installing company1..."
-helm install company1 --set nameOverride=company1 ./helm-charts/company
+helm install company1 --set nameOverride=company1,ports.debug.port=5005 ./helm-charts/company
 
 echo "installing company2..."
-helm install company2 --set nameOverride=company2 ./helm-charts/company
+helm install company2 --set nameOverride=company2,ports.debug.port=5006 ./helm-charts/company
 
 echo "installing company3..."
-helm install company3 --set nameOverride=company3 ./helm-charts/company
+helm install company3 --set nameOverride=company3,ports.debug.port=5007 ./helm-charts/company
 
 echo "installing newman..."
 helm install newman ./helm-charts/newman
@@ -25,11 +25,9 @@ helm install cli-tools ./helm-charts/cli-tools
 
 echo "installing company1 data dashboard..."
 helm install company1-datadashboard --set nameOverride=company1-datadashboard,companyName=company1 ./helm-charts/company-dashboard
-#helm template company1-datadashboard --set nameOverride=company1-datadashboard,companyName=company1 ./helm-charts/company-dashboard
 
 echo "installing company2 data dashboard..."
 helm install company2-datadashboard --set nameOverride=company2-datadashboard,companyName=company2 ./helm-charts/company-dashboard
-#helm template company2-datadashboard --set nameOverride=company2-datadashboard,companyName=company2 ./helm-charts/company-dashboard
 
 echo "installing company3 data dashboard..."
 helm install company3-datadashboard --set nameOverride=company3-datadashboard,companyName=company3 ./helm-charts/company-dashboard
