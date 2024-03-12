42d74ed6de73fce6f351e4f9697716a38b33c886
farhin23 <farhin.jahan@isst.fraunhofer.de>
2024-03-12 23:18:36 +0100

    Update docker image version format

--- system-tests/helm/kind-run.sh
+++ system-tests/helm/kind-run.sh
@@ -4,7 +4,7 @@ echo "creating new cluster..."
 kind create cluster --config=kind-cluster.yaml
 
 echo "load the images..."
-kind load docker-image edc-connector-dashboard-company1:v.01 edc-connector-dashboard-company2:v.01 edc-connector-dashboard-company3:v.01 edc-connector:v.01 registration-service:v.01 cli-tools:v.01
+kind load docker-image edc-connector-dashboard-company1:v0.2.0 edc-connector-dashboard-company2:v0.2.0 edc-connector-dashboard-company3:v0.2.0 edc-connector:v0.2.0 registration-service:v0.2.0 cli-tools:v0.2.0
 
 echo "apply ingress..."
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
